dd9be848c73bedf62c0a2f08148674df6c39184d
Andrea <Andrea>
2016-11-09 00:06:14 -0300

    me estoy durmiendo. mañana temprano lo termino mathi :(

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ function addLista(){
       contInput.classList.add("panel","panel-primary");
   var inputLista = document.createElement('input');
       inputLista.setAttribute("placeholder","Añadir una Lista...");
+      inputLista.setAttribute("id","guardarLista")
       inputLista.classList.add("form-control","inputShadow");
  // var textoInput = document.createTextNode('Añadir una Lista...');
 
@@ -18,6 +19,20 @@ function addLista(){
   añadirLista.appendChild(contInput);
   //añadirLista.appendChild(inputLista);
   //añadirLista.appendChild(btnLista);
-  
+  btnLista.addEventListener("click",function(){
+    
+    var resultLista = document.getElementById("guardarLista").value;
+    var nuevoTexto=document.createElement('h3');
+    var resultSpan=document.createTextNode(resultLista);
+    nuevoTexto.appendChild(nuevoSpan);
+    var rowLista = document.getElementById("rowLista");
+    var columnaNueva = document.createElement("div");
+        columnaNueva.classList.add("col-xs-12","col-sm-12","col-md-3")
 
+    var contTarea = document.createElement("div");
+        contTarea.setAttribute("id","contTarea");
+        contTarea.classList.add("panel","panel-primary");
+        columnaNueva.appendChild(contTarea);
+        rowLista.appendChild(columnaNueva);
+  })
 }
